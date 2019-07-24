dd7f94b1a4d1b0a1cd391d6164ff30e6b2b492b6
Michael Roberson <michaelroberson@gmail.com>
2019-07-23 21:10:17 -0400

    Add filter fetch command. Add filter directory to .gitignore.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -32,16 +33,52 @@ type filter struct {
 	SHA256       string        `json:"sha256,omitempty"`
 }
 
-func usage(progName string) {
-	fmt.Printf("usage: %s <calculate|lookup|fromfile|filters> <filterfile> <file1> [file2 ...]\n", progName)
-	os.Exit(1)
+func isMD5(value string) {
+	return
+}
+
+func md5First8192(filename string) {
+	return
+}
+
+func md5File(filename string) {
+	return
+}
+
+func countFiles(path string) {
+	return
+}
+
+func calculateHashes() {
+	return
 }
 
 func calculate() {
 	return
 }
 
+func lookupHashes() {
+	return
+}
+
 func lookup() {
+	return
+}
+
+func fromfile() {
+	return
+}
+
+func usage(progName string) {
+	fmt.Printf("usage: %s <calculate|lookup|fromfile|filters> <filterfile> <file1> [file2 ...]\n", progName)
+	os.Exit(1)
+}
+
+func readableFile(path string) {
+	return
+}
+
+func writeableFile(path string) {
 	return
 }
 
@@ -69,17 +106,68 @@ func getConfig() mddConfig {
 	return config
 }
 
+func updateMetadata(baseURL string) map[string]filter {
+	var client = &http.Client{Timeout: 10 * time.Second}
+	url := baseURL + "METADATA.json"
+	response, err := client.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	content, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var metadata map[string]filter
+	if unmarshalErr := json.Unmarshal(content, &metadata); unmarshalErr != nil {
+		log.Fatalf("JSON unmarshaling failed: %s", unmarshalErr)
+	}
+	writeErr := ioutil.WriteFile("METADATA.json", content, 0644)
+	if writeErr != nil {
+		log.Fatal(writeErr)
+	}
+	return metadata
+}
+
+// Possibly not needed?
+func getMetadata() map[string]filter {
+	path := filepath.Join(currentDir(), "METADATA.json")
+	content, readErr := ioutil.ReadFile(path)
+	if readErr != nil {
+		config := getConfig()
+		metadata := updateMetadata(config.Repo)
+		return metadata
+	}
+	var metadata map[string]filter
+	if unmarshalErr := json.Unmarshal(content, &metadata); unmarshalErr != nil {
+		log.Fatalf("JSON unmarshaling failed: %s", unmarshalErr)
+	}
+	return metadata
+}
+
+func hasher(hashAlgorithm string) {
+	return
+}
+
+func currentDir() string {
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	dir := filepath.Dir(ex)
+	return dir
+}
+
+func filterPath() string {
+	path := filepath.Join(currentDir(), "filters")
+	return path
+}
+
 func getInstalled(hashAlg string) map[string]filter {
 	content, readErr := ioutil.ReadFile("installed.json")
 	if readErr != nil {
 		var installed map[string]filter
-		ex, err := os.Executable()
-		if err != nil {
-			panic(err)
-		}
-		dir := filepath.Dir(ex)
-		path := filepath.Join(dir, "filters")
-		fileInfo, err := ioutil.ReadDir(path)
+		fileInfo, err := ioutil.ReadDir(filterPath())
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -134,48 +222,88 @@ func listLocal(hashAlg string) {
 	printFilters(installed)
 }
 
-func updateMetadata(baseURL string) map[string]filter {
-	var client = &http.Client{Timeout: 10 * time.Second}
-	url := baseURL + "METADATA.json"
-	response, err := client.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer response.Body.Close()
-	content, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var metadata map[string]filter
-	if unmarshalErr := json.Unmarshal(content, &metadata); unmarshalErr != nil {
-		log.Fatalf("JSON unmarshaling failed: %s", unmarshalErr)
-	}
-	writeErr := ioutil.WriteFile("METADATA.json", content, 0644)
-	if writeErr != nil {
-		log.Fatal(writeErr)
-	}
-	return metadata
-}
-
 func listRemote(repoURL string) {
 	remote := updateMetadata(repoURL)
 	printFilters(remote)
 }
 
-func isInstalled() {
-	return
+func isInstalled(target string) bool {
+	config := getConfig()
+	installed := getInstalled(config.HashAlg)
+	_, inInstalledFile := installed[target]
+	path := filepath.Join(filterPath(), target)
+	fileExists := true
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		fileExists = false
+	}
+	return inInstalledFile && fileExists
 }
 
-func downloadFilter() {
-	return
+func downloadFilter(target string) {
+	config := getConfig()
+	url := config.Repo + target
+	fmt.Printf("Fetching %s...\n", target)
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("%s filter not found!\n", target)
+		os.Exit(1)
+	}
+	defer response.Body.Close()
+	path := filepath.Join(filterPath(), target)
+	filter, err := os.Create(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer filter.Close()
+	_, err = io.Copy(filter, response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func updateInstalled() {
-	return
+func updateInstalled(target string) {
+	config := getConfig()
+	metadata := updateMetadata(config.Repo)
+	installed := getInstalled(config.HashAlg)
+	targetData := metadata[target]
+	var digest string
+	switch config.HashAlg {
+	case "md5":
+		digest = targetData.MD5
+	case "sha1":
+		digest = targetData.SHA1
+	case "sha256":
+		digest = targetData.SHA256
+	default:
+		log.Fatalf("Invalid hash algorithm in config.json: %s", config.HashAlg)
+	}
+	var hash = hashAlgorithm{
+		Alg:    config.HashAlg,
+		Digest: digest,
+	}
+	installed[target] = filter{
+		Description:  targetData.Description,
+		LastModified: targetData.LastModified,
+		Hash:         hash,
+	}
+	path := filepath.Join(currentDir(), "installed.json")
+	data, marshalErr := json.MarshalIndent(installed, "", "    ")
+	if marshalErr != nil {
+		log.Fatalf("JSON marshaling failed: %s", marshalErr)
+	}
+	writeErr := ioutil.WriteFile(path, data, 0644)
+	if writeErr != nil {
+		log.Fatal(writeErr)
+	}
 }
 
-func fetchFilter() {
-	return
+func fetchFilter(target string) {
+	if isInstalled(target) {
+		fmt.Printf("%s is already installed\n", target)
+	} else {
+		downloadFilter(target)
+		updateInstalled(target)
+	}
 }
 
 func updateFilters() {
@@ -186,7 +314,12 @@ func filters(args []string) {
 	command := args[2]
 	switch command {
 	case "fetch":
-		fetchFilter()
+		if len(args) > 3 {
+			target := args[3]
+			fetchFilter(target)
+		} else {
+			usage(args[0])
+		}
 	case "list":
 		config := getConfig()
 		if len(args) > 3 {
@@ -205,10 +338,6 @@ func filters(args []string) {
 		fmt.Printf("Invalid command: %s\n", command)
 		usage(args[0])
 	}
-}
-
-func fromfile() {
-	return
 }
 
 func main() {
